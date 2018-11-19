518e0843989f38ae313a83cb75b2dda847544b5e
Soham <sohampardeshi@gmail.com>
2018-11-18 19:23:30 -0800

    Added code for getting 10-K URLs

--- company.py
+++ company.py
@@ -1,16 +1,36 @@
-import requests
+import requests, zipfile, io
+from math import ceil
 
 
 # Represents a company in the EDGAR database
 class Company():
+    ARCHIVE_URL = "https://www.sec.gov/Archives/"
 
     def __init__(self, name, cik):
         self.name = name
         self.cik = cik
 
-    # Returns the URL for this filing
-    def getFilingsUrl(self, filingType="10-K", date=""):
-        return ""
+    # Returns a list of the URL for this filing type in the given year
+    def getFilingsUrl(self, filingType="10-K", fyear="2005"):
+
+        # Find the index file that contains to this file in each quarter
+        output = []
+        for q in range(1, 5):
+            fquarter = "QTR" + str(q)
+            index_url = Company.ARCHIVE_URL + "edgar/full-index/" + fyear + "/" + fquarter + "/master.zip"
+
+            # Download and extract the zip file
+            index_zip = requests.get(index_url)
+            tmp = zipfile.ZipFile(io.BytesIO(index_zip.content))
+            index_file = tmp.read('master.idx').decode("utf-8")
+
+            # Search index file for entry matching this
+            for line in index_file.splitlines()[11:]:
+                data = line.split("|")
+                if int(self.cik) == int(data[0]) and ("10-K" in data[2]):
+                    output.append(Company.ARCHIVE_URL + data[4])
+
+        return output
 
     # Returns the HTML content of a filing for this company
     def getFilings(self, filingType="10-K", date=""):
--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from edgar_utils import EdgarLookup
 from company import Company
-import sys
+import csv
 
 if __name__ == "__main__":
-    e = EdgarLookup()
-    c = Company("ORACLE CORP", e.lookup["ORACLE CORP"])
+    #e = EdgarLookup()
+    """with open('funda.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count != 0:
+                # Get company
+                gvkey = row[0]
+                fyear = row[1]
+                datadate = row[2]
+                LatestPossibleFilingDate = row[3]
+                cik = row[4]
+                conm = row[5]
 
+                print("Considering " + row[5] + " on " + row[2])
+                c = Company(row[5], row[4])
+                c.getFilingsUrl(filingType="10-K", date=row[2], fyear=row[1])
+
+            line_count += 1
+    """
+    c = Company("ALPHARMA -CL A", "0000730469")
+    x = c.getFilingsUrl(filingType="10-K")
+    print(x)
 
     # GV Key, FYear, Data Date, CIK, Company Name
