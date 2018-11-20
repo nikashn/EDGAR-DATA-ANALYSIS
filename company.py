411663f2ec6cb200c508ddab7c8189f897fd852b
Nikash Narula <nikashn@gmail.com>
2018-11-19 16:04:24 -0800

    Save changes

--- company.py
+++ company.py
@@ -1,5 +1,5 @@
 import requests, zipfile, io
-from math import ceil
+import gzip
 
 
 # Represents a company in the EDGAR database
@@ -22,12 +22,13 @@ class Company():
             # Download and extract the zip file
             index_zip = requests.get(index_url)
             tmp = zipfile.ZipFile(io.BytesIO(index_zip.content))
-            index_file = tmp.read('master.idx').decode("utf-8")
+            index_file = tmp.read('master.idx').decode(encoding="UTF-8", errors='replace')
+
 
             # Search index file for entry matching this
             for line in index_file.splitlines()[11:]:
                 data = line.split("|")
-                if int(self.cik) == int(data[0]) and (filingType in data[2]):
+                if (int(self.cik) == int(data[0])) and (filingType in data[2]):
                     output.append(Company.ARCHIVE_URL + data[4])
 
         return output
@@ -36,6 +37,29 @@ class Company():
     def getFilingsFromURL(self, filingType, fyear):
         # Get the files for this filingType on this fiscal year
         urls = self.getFilingsURLs(filingType, fyear)
+
+        # Download all of the files
+        files = []
+        for url in urls:
+            c = requests.get(url).content
+            files.append(c)
+
+        return files
+
+    # Returns the HTML content of a filing for this company
+    def getFilingsFromFile(self, filingType, fyear):
+
+        urls = [] # URLs for all the 10-Ks
+        for q in range(1, 5):
+            # Read the local index file for this filingType on this fiscal year
+            path = "./index_files/master_index_" + fyear + "_QTR" + str(q) + ".idx"
+            index_file = open(path, "r").read()
+
+            # Search this index file for entry matching the filing we want
+            for line in index_file.splitlines()[11:]:
+                data = line.split("|")
+                if (int(self.cik) == int(data[0])) and (filingType in data[2]):
+                    urls.append(Company.ARCHIVE_URL + data[4])
 
         # Download all of the files
         files = []
--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from edgar_utils import EdgarLookup
 from company import Company
+from bs4 import BeautifulSoup
 import csv
 
 if __name__ == "__main__":
 
-    # e = EdgarLookup()
     with open('funda.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
 
+        # 0 = gvkey, 1 = fyear, 2 = datadate, 3 = LatestPosible, 4 = CIK, 5 = Name
         for row in csv_reader:
             if line_count != 0:
 
-                # 0 = gvkey, 1 = fyear, 2 = datadate, 3 = LatestPosible, 4 = CIK, 5 = Name
+                # Print header
                 print("Considering " + row[5] + " from " + row[1])
+
+                # Get all the files for this company
                 c = Company(row[5], row[4])
-                files = c.getFilingsURLs(filingType="10-K", fyear=row[1])
-                print("\t" + str(files))
+                files = c.getFilingsFromFile(filingType="10-K", fyear=row[1])
+
+                # Process each file
+                factors = []
+                for file in files:
+                    soup = BeautifulSoup(file, 'html.parser')
+                    print(soup.title)
 
             line_count += 1
 
-
-    # GV Key, FYear, Data Date, CIK, Company Name
+    print("Completed!")
