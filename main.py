64a3e5983873e5a838e8bce8d7ab7ac988cc0a90
Nikash Narula <nikashn@gmail.com>
2018-12-21 19:39:35 -0800

    count num RF

--- main.py
+++ main.py
@@ -4,45 +4,46 @@ from bs4 import BeautifulSoup
 import csv
 
 def funda_test():
-    with open('funda.csv') as csv_file:
+    with open('funda_20181025_updated.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
 
-        # 0 = gvkey, 1 = fyear, 2 = datadate, 3 = LatestPosible, 4 = CIK, 5 = Name
+        '''
+        0 = gvkey,
+        1 = fyear,
+        2 = datadate,
+        3 = LatestPosible,
+        4 = CIK,
+        5 = Name
+        '''
+        next(csv_reader, None)
+        #count = 0
         for row in csv_reader:
-            if line_count != 0:
+        #    if count > 20:
+        #        return
+        #    else:
+        #        count += 1
+            # Print header
+            print(row[5] + " " + row[2] + " to " + row[3] + ":")
 
-                # Print header
-                print("Considering " + row[5] + " from " + row[1])
+            # Get all the files for this company
+            c = Company(row[5], row[4])
+            files, urls = c.getFilingsFromURL("10-K", row[2], row[3])
 
-                # Get all the files for this company
-                c = Company(row[5], row[4])
-                files = c.getFilingsFromFile(filingType="10-K", fyear=row[1])
+            # Process each file
+            print("\t[found " + str(len(files)) + " files]")
+            num_zeroes = 0
+            for i in range(len(files)):
+                print("\t[" + urls[i] + ":", end='\t')
+                soup = BeautifulSoup(files[i], "html.parser")
+                the_risk_factors = soup.select('p b i font')
+                if len(the_risk_factors) == 0:
+                    num_zeroes += 1
+                print(str(len(the_risk_factors)) + "]")
+            print()
 
-                # Process each file
-                factors = []
-                for file in files:
-                    soup = BeautifulSoup(file, 'html.parser')
-                    print(soup.find_all("html"))
+        percent_zeroes = num_zeroes/len(files) * 100
+        print(percent_zeroes, '%') 
 
-            line_count += 1
 
 if __name__ == "__main__":
-
-    c = Company("AAR CORP", "0000001750")
-    files = c.getFilingsFromURL("10-K", "5/31/06", "9/5/06")
-
-    # Find all the 10-K's in the range
-    for file in files:
-       c = Company("AAR CORP", "0000001750")
-    files = c.getFilingsFromURL("10-K", "5/31/06", "9/5/06")
-
-    # Find all the 10-K's in the range
-    for file in files:
-        soup = BeautifulSoup(file, "html.parser")
-        the_risk_factors = soup.select('p b i font')
-        print(len(the_risk_factors))
-        #i = 1
-        #for risk_factor in the_risk_factors:
-        #    print('\n\n' + str(i) + '.\t' + risk_factor.text + '\n\n')
-        #    i += 1
+    funda_test()
